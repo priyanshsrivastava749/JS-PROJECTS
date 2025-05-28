9e1779e4f14e517b523090303e7ccd3a552cd49c
priyanshsrivastava749 <priyanshsrivastava749@gmail.com>
2025-05-28 23:56:42 +0530

    removed unnecessary files

--- Practice_projects/FIRST/script.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-document.querySelector(".add-btn").addEventListener("click", () => {
-  count++;
-  document.querySelector(".items").innerText = `Your cart has ${count} items in it`;
-});
-document.querySelector(".remove-btn").addEventListener("click",() => {
-  if(count>0)count--;
-  document.querySelector(".items").innerText = `Your cart has ${count} items in it`;
-})
